c2b448d04e5fcfb954a2b2a4df8bb095cfba65d1
Diego Mernies <diego.mernies@securitize.io>
2024-10-31 11:59:49 -0300

    fix: fixing database model

--- src/health-orders/health-order-result.entity.ts
+++ src/health-orders/health-order-result.entity.ts
@@ -3,7 +3,7 @@ import { HealthOrder } from './health-order.entity';
 
 @Entity({ name: 'health_order_results' })
 export class HealthOrderResult {
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', length: 36 })
   @PrimaryGeneratedColumn('uuid')
   public id: string;
 
@@ -13,6 +13,9 @@ export class HealthOrderResult {
 
   @Column({ name: 'file_data', type: 'mediumblob', nullable: true })
   public fileData?: Buffer;
+
+  @Column({ name: 'additional_notes', type: 'text', nullable: true })
+  public additionalNotes?: string;
 
   @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
   public createdAt: Date;
